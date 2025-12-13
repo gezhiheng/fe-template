51778a0b2223cdd053d15ce0c39e254d3a420875
Henry Ge <54823984+gezhiheng@users.noreply.github.com>
2025-12-13 15:10:30 +0800

    feat: add unplugin-vue-router for automatic route generation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { handleHotUpdate, routes } from 'vue-router/auto-routes'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  history: createWebHistory(),
+  routes,
 })
+
+// This will update routes at runtime without reloading the page
+if (import.meta.hot) {
+  handleHotUpdate(router)
+}
 
 export default router
